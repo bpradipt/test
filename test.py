e273fe6a43b485663247a277cb3aa9c636e4f489
Pradipta Kumar Banerjee <bpradipt@in.ibm.com>
2016-03-10 14:38:23 +0000

    change access code

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ while True:
     try:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(host, port=15383, username='test', password='llm123')
+        ssh.connect(host, port=15383, username='ubuntu', password='passw0rd')
         print "Connected to %s" % host
         break
     except paramiko.AuthenticationException:
